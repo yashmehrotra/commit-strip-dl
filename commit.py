2ac2a54c393e3eccb1b1e725fcabc71cdb9bce99
Yash Mehrotra <yashmehrotra95@gmail.com>
2014-12-14 23:16:17 +0530

    fixed uri_encoding

--- commit.py
+++ commit.py
@@ -1,11 +1,45 @@
 import requests
 import urllib
+import urlparse
 import re
 from lxml import html
 import pdb
 import os
 
 BASE_URL = 'http://www.commitstrip.com/en/{0}/{1}/page/{2}'
+
+def fixurl(url):
+    # turn string into unicode
+    if not isinstance(url,unicode):
+        url = url.decode('utf8')
+
+    # parse it
+    parsed = urlparse.urlsplit(url)
+
+    # divide the netloc further
+    userpass,at,hostport = parsed.netloc.rpartition('@')
+    user,colon1,pass_ = userpass.partition(':')
+    host,colon2,port = hostport.partition(':')
+
+    # encode each component
+    scheme = parsed.scheme.encode('utf8')
+    user = urllib.quote(user.encode('utf8'))
+    colon1 = colon1.encode('utf8')
+    pass_ = urllib.quote(pass_.encode('utf8'))
+    at = at.encode('utf8')
+    host = host.encode('idna')
+    colon2 = colon2.encode('utf8')
+    port = port.encode('utf8')
+    path = '/'.join(  # could be encoded slashes!
+        urllib.quote(urllib.unquote(pce).encode('utf8'),'')
+        for pce in parsed.path.split('/')
+    )
+    query = urllib.quote(urllib.unquote(parsed.query).encode('utf8'),'=&?/')
+    fragment = urllib.quote(urllib.unquote(parsed.fragment).encode('utf8'))
+
+    # put it back together
+    netloc = ''.join((user,colon1,pass_,at,host,colon2,port))
+    return urlparse.urlunsplit((scheme,netloc,path,query,fragment))
 
 def create_dir(year, month):
     
@@ -42,6 +76,7 @@ def download(year, month):
         try:
             pdb.set_trace()
             img_url = tree.xpath('//div[@class="entry-content"]/p/img/@src')[0]
+            img_url = fixurl(img_url)
             title = tree.xpath( '//h1[@class="entry-title"]/a/text()')[0].encode('utf-8').decode('ascii','ignore')
             print 'Downloading {0}'.format(title)
             urllib.urlretrieve(img_url,title+'.jpg')
